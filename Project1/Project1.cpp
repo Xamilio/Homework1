a0003faed4ec5bac7f9709b092a52290f89e9ab3
user <user@WIN-4HU5T7LM44J>
2023-04-19 14:31:38 +0300

    Добавьте файлы проекта.

---
+++ Project1/Project1.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	cout << "Mon Tue Wed Thu Fri Sat Sun \n";
+	cout << "                     1   2\n";
+	cout << " 3   4   5   6   7   8   9\n";
+	cout << " 10  11  12  13  14  15  16\n";
+	cout << " 17  18  19  20  21  22  23\n";
+	cout << " 24  25  26  27  28  29  30\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+
+
+
+
+
+							 
+	cout << "     *****             ****  **   **   **           **** \n";
+	cout << "   *********          *****  **   **   **          ***** \n";
+	cout << "  ***     ***        **  **  **   **   **         **  ** \n";
+	cout << " ***                **   **  **   **   **        **   ** \n";
+	cout << " ***               ********  **   **   **       ******** \n";
+	cout << " ***              *********  **   **   **      ********* \n";
+	cout << "  ***     ***    **      **  **   **   **     **      ** \n";
+	cout << "   *********    **       **  ************    **       ** \n";
+	cout << "     *****     **        **  ************   **        ** \n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+
+
+
+
+
+
+
+
+	cout << "          *    **     \n";
+	cout << "       *     * **	    \n";
+	cout << "    *           *     \n";
+	cout << " *                 *	\n";
+	cout << " * * * * * * * * * *     \n";
+	cout << " *                 *     \n";
+	cout << " *                 *     \n";
+	cout << " *  ****           *     \n";
+	cout << " *  *  *       *****    \n";
+	cout << " *  ****       *   *  * * * * * * * * * * * \n";
+	cout << " *             *   *  * * * * * * * * * * * \n";
+	cout << " *             *   *  * * * * * * * * * * * \n";
+	cout << " * * * * * * * * * *  * * * * * * * * * * * \n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+
+	cout << "      *\n";
+	cout << "\n";
+	cout << "*         *                       *\n";
+	cout << "                  *\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "                      *         *\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+
+
+
+
+
+}
+	
